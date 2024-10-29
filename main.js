2e5acded895ed3688da7305aaea919b2bba91935
MrLexx1 <muhilman606@gmail.com>
2024-10-29 10:35:38 +0800

    Update

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
-let result = "";
-for (let i = 1; i <= 4; i++) {
-  for (let j = 1; j <= 4; j++) {
-    result += j;
-  }
-  result += "\n";
-}
-console.log(result);
+// let result = "";
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 1; j <= 4; j++) {
+//     result += j;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     result += j;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 1; j <= 4; j++) {
+//     result += i;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// for (let i = 0; i <= 4; i++) {
+//   for (let j = 1; j <= 5; j++) {
+//     result += j + i;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     result += " ";
+//   }
+//   for (let j = i; j <= 4; j++) {
+//     result += j;
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// let result = "";
+// let baris = ["12345", "678910", "98765", "43210"];
+
+// for (let i = 0; i < baris.length; i++) {
+//   result += baris[i] + "\n";
+// }
+
+// console.log(result);
